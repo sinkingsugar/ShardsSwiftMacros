47e260a1ce58ef4a85c99bb91522b267c39d7c0d
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2025-08-22 19:19:49 +0800

    Creating file ShardParamMacro.swift

---
+++ Sources/ShardsSwiftMacrosPlugin/ShardParamMacro.swift
@@ -0,0 +1,51 @@
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import SwiftSyntaxMacros
+
+/// The @ShardParam macro for marking shard parameters
+public struct ShardParamMacro: PeerMacro {
+    public static func expansion(
+        of node: AttributeSyntax,
+        providingPeersOf declaration: some DeclSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> [DeclSyntax] {
+        // This macro is primarily used for metadata by the @Shard macro
+        // It doesn't generate additional code by itself
+        return []
+    }
+}
+
+/// The @ShardParameters macro for generating parameter classes
+public struct ShardParametersMacro: DeclarationMacro {
+    public static func expansion(
+        of node: some FreestandingMacroExpansionSyntax,
+        in context: some MacroExpansionContext
+    ) throws -> [DeclSyntax] {
+        
+        guard let arguments = node.arguments,
+              arguments.count >= 1,
+              let nameExpr = arguments.first?.expression.as(StringLiteralExprSyntax.self),
+              let nameSegment = nameExpr.segments.first?.as(StringSegmentSyntax.self) else {
+            throw MacroError.invalidArguments
+        }
+        
+        let className = nameSegment.content.text
+        
+        // Generate a basic parameters class structure
+        return [
+            """
+            class \(raw: className) {
+                static let shared = \(raw: className)()
+                
+                let parameters: Parameters
+                
+                init() {
+                    parameters = .init()
+                    // TODO: Add parameter definitions here
+                    parameters.done()
+                }
+            }
+            """
+        ]
+    }
+}
